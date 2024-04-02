de580e1a8dd382afa6e7c75ca593ebeb38080363
gunners <chakagae123@naver.com>
2024-04-02 18:13:31 +0900

    * insert 쿼리

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -8,9 +8,23 @@ public class JpaMain {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        //code
+        try {
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("AAA");
+
+            em.persist(member);
+
+            tx.commit();
+        } catch(Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
         emf.close();
     }
 }
