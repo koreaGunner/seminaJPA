d36a398921982bf12827c2dc1634327368057ff1
gunners <chakagae123@naver.com>
2024-04-02 22:01:20 +0900

    * CRUD Test

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -13,11 +13,31 @@ public class JpaMain {
         tx.begin();
         //code
         try {
-            Member member = new Member();
-            member.setId(1L);
-            member.setName("AAA");
+            //1. insert
+//            Member member = new Member();
+//            member.setId(1L);
+//            member.setName("AAA");
+//            System.out.println("=========");
+//            em.persist(member);
+//            System.out.println("=========");
 
-            em.persist(member);
+            //2. select
+//            Member member = em.find(Member.class, 1L);
+//
+//            System.out.println("member.getName() = " + member.getName());
+
+            //3. update
+//            Member member = em.find(Member.class, 1L);
+//            member.setName("BBB");
+//
+//            em.persist(member);
+
+            //4. delete
+//            Member member = em.find(Member.class, 1L);
+//
+//            System.out.println("=========");
+//            em.remove(member);
+//            System.out.println("=========");
 
             tx.commit();
         } catch(Exception e) {
